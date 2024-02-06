32c7445376f3fb4a6f3ac35a90e779e48ed3710a
DariaKaraulkina <D.morgunovaa@mail.ru>
2024-02-06 16:19:56 +0300

    ПОЛУЧИЛОСЬ

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 /*Добавляем функционал. Теперь кол-во котиков тоже задаём с консоли. Для этого пишем в консоль сообщение
         "Введите размер массива котиков (сколько их будет): " и вводим размер массива. Далее по аналогии с предыдущей
         реализацией вводим в консоль сами проценты сытости для каждого котика. Ввод каждого из значений нужно предварять
@@ -6,12 +5,11 @@
 
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(quantity()));
-        System.out.println(Arrays.toString(catsHangry()));
     }
+
     public static int[] quantity() {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите размер массива котиков (сколько их будет):");
@@ -20,25 +18,20 @@ public class Main {
         for (int i = 0; i <= (R - 1); i++) {
             myArray[i] = i + 1;
         }
-        return myArray;
-    }
-    public static int[] catsHangry() {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int[] myArray = new int[N];
-        myArray[N] = in.nextInt();
-        for (int i = 0; i < myArray.length; i++) {
-            System.out.print("Введите процент сытости" + N +"го котика:");
-                if (myArray[i] < 100) {
-                    myArray[i] = i + (100 - i);
-                } else if (myArray[i] > 100) {  //ошибка входных данных, котик полностью сыт
-                    myArray[i] = 100;
-                } else
-                    myArray[i] = 100;
-            }
-            return myArray;
+
+        for (int j = 0; j < myArray.length; j++) {
+            System.out.println("Введите процент сытости " + j + "го котика: ");
+            int N = in.nextInt();
+            if (myArray[j] < 100) {
+                myArray[j] = j + (100 - j);
+            } else if (myArray[j] > 100) {  //ошибка входных данных, котик полностью сыт
+                myArray[j] = 100;
+            } else
+                myArray[j] = 100;
         }
+        return  myArray;
     }
+}
 
 
 
